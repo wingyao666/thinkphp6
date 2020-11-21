700baa6de816472999e36a20bbfde8a57652cab6
wingyao <350577790@qq.com>
2020-11-21 20:48:32 +0800

    init

---
+++ app/controller/Event.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+
+class Event extends BaseController
+{
+    public function index()
+    {
+        return 66666;
+    }
+
+    public function hello($name = 'ThinkPHP6')
+    {
+
+    }
+}
